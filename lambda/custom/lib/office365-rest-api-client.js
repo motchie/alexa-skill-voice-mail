ad811fae33d93bdddc3fd968aad4521cc1282dc5
Toru Motchie MOCHIDA <motchie@motchie.com>
2018-01-16 01:45:04 +0900

    エラー出力の修正と未読メール読み出し機能の追加

--- lambda/custom/lib/office365-rest-api-client.js
+++ lambda/custom/lib/office365-rest-api-client.js
@@ -25,10 +25,49 @@ function unReadMailCount() {
                     (res) => { resolve(res.value.length); }
                 )
                 .catch(
-                    (err) => { reject(new Error(err)) }
+                    (err) => { reject(console.log(err)) }
                 );
         });
 };
 
+function UnReadMails() {
+    return new Promise(
+        (resolve, reject) => {
+            client
+                .api('/me/mailfolders/inbox/messages')
+                .filter("isRead eq false")
+                .select("id", "from", "subject", "bodyPreview", "receivedDateTime")
+                .get()
+                .then(
+                    (res) => { resolve(processingEMails(res)); }
+                ).catch(
+                    (err) => {
+                        reject(console.log(err));
+                    }
+                );
+        });
+}
+
+function processingEMails(rawEmails) {
+    const emails = [];
+
+    for (let rawEmail of rawEmails.value) {
+        const email = {};
+
+        email.id = rawEmail.id;
+        email.from = rawEmail.from.emailAddress.name;
+        email.subject = rawEmail.subject;
+        email.body = rawEmail.bodyPreview;
+        email.body = email.body.replace(/\r\n+/ig, "");
+        email.body = email.body.replace(/--+/ig, "");
+        email.body = email.body.replace(/  +/ig, "");
+        email.received = rawEmail.receivedDateTime;
+
+        emails.push(email);
+    }
+    return emails;
+}
+
 module.exports.setAccessToken = setAccessToken;
 module.exports.unReadMailCount = unReadMailCount;
+module.exports.UnReadMails = UnReadMails;
