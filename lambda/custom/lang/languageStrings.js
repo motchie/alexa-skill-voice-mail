5ff3e07aabde4fc36eca0e727b86c572364058c0
Toru Motchie MOCHIDA <motchie@motchie.com>
2018-02-07 01:31:56 +0900

    ask にプロンプトを付加。

--- lambda/custom/lang/languageStrings.js
+++ lambda/custom/lang/languageStrings.js
@@ -3,17 +3,17 @@ const languageStrings = {
         'translation': {
             'PLEASE_LINK_ACCOUNT': 'このスキルを利用するには、アカウントリンクが必要です。Alexaアプリでアカウントリンクを設定してください。',
             'WELCOME_TO_VOICEMAIL': '音声メールです。',
-            'THERE_ARE_UNREAD_MESSAGES': '%s 通の未読メールがあります。<break time=\'1s\'/>',
-            'NO_UNREAD_MESSAGES': '未読メールはありません。',
-            'INVALID_DATE': '日付が認識できませんでした。発話を変更してお試しください。',
-            'FUTURE_DATE': '未来の日付として認識されました。発話を変更してお試しください。',
-            'NO_MESSAGES': '%s にはメールが届いていません。',
-            'THERE_ARE_MESSAGES': '%s には %s 通のメールがあります。<break time=\'1s\'/>',
+            'THERE_ARE_UNREAD_MESSAGES': '%s 通の未読メールがあります。<break time=\'1s\'/>発話で指示をお願いします。',
+            'NO_UNREAD_MESSAGES': '未読メールはありません。<break time=\'1s\'/>発話で指示をお願いします。',
+            'INVALID_DATE': '日付が認識できませんでした。発話を変更してお試しください。<break time=\'1s\'/>どうぞ。',
+            'FUTURE_DATE': '未来の日付として認識されました。発話を変更してお試しください。<break time=\'1s\'/>どうぞ。',
+            'NO_MESSAGES': '%s にはメールが届いていません。<break time=\'1s\'/>どうぞ。',
+            'THERE_ARE_MESSAGES': '%s には %s 通のメールがあります。<break time=\'1s\'/>どうぞ。',
             'SAY_SOMETHING': '何か指示をお願いします。',
-            'CANCEL': 'キャンセルしました',
+            'CANCEL': 'キャンセルしました<break time=\'1s\'/>どうぞ。',
             'HELP': 'メールを音声で聞くことができます。「未読メール」と発話すると未読メールを、「今日のメール」「昨日のメール」などと発話すると、該当する日のメールの件名、受信日付、本文の先頭250文字程度を読み上げます。では、発話してみてください。',
             'BYE': 'さようなら。',
-            'unhandled': 'すみません、処理できない内容でした。',
+            'unhandled': 'すみません、処理できない内容でした。<break time=\'1s\'/>どうぞ。',
         }
     }
 };
